3244c4be8da5b014ddc042e1ec9e8efc939e2468
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2025-07-15 17:25:47 +0200

    ADD/lesson-10 keep alive , dynamic components and teports

--- basics/lesson-10/src/App.vue
+++ basics/lesson-10/src/App.vue
@@ -1,33 +1,40 @@
 <template>
 	<div>
 		<the-header></the-header>
-		<badge-list></badge-list>
-		<user-info
-			:full-name="activeUser.name"
-			:info-text="activeUser.description"
-			:role="activeUser.role"
-		></user-info>
-		<course-goal #default="props">
-			<p>{{ props.item }}</p>
-		</course-goal>
+
+		<button @click="setSelectedComponent('active-goals')">ACtive Goals</button>
+		<button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+		<keep-alive>
+			<component :is="activeGoals"></component>
+		</keep-alive>
 	</div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoal from './components/CourseGoal.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoal from './components/CourseGoal.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
 	components: {
 		TheHeader,
-		BadgeList,
-		UserInfo,
-		CourseGoal,
+		// BadgeList,
+		// UserInfo,
+		// CourseGoal,
+		ActiveGoals,
+		ManageGoals,
+	},
+	methods: {
+		setSelectedComponent(name) {
+			this.activeGoals = name;
+		},
 	},
 	data() {
 		return {
+			activeGoals: 'active-goals',
 			activeUser: {
 				name: 'Maximilian Schwarzmüller',
 				description: 'Site owner and admin',
