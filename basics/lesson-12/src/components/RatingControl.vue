cf3960a505040ac3604168a332b5e680349f3d9d
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2025-07-23 16:38:33 +0200

    ADD/lesson-12 forms

---
+++ basics/lesson-12/src/components/RatingControl.vue
@@ -0,0 +1,62 @@
+<template>
+  <ul>
+    <li :class="{ active: activeOption === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: activeOption === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: activeOption === 'greate' }">
+      <button type="button" @click="activate('greate')">Greate</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  computed: {
+    activeOption() {
+      return this.modelValue;
+    },
+  },
+  methods: {
+    activate(option) {
+      this.activeOption = option;
+      this.$emit('update:modelValue', option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: pink;
+}
+.active button {
+  background-color: pink;
+  color: white;
+}
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
